afa6babcb73f111be57a4b3a9db8c35a24df3b26
snorresovold <snorresovold@gmail.com>
2021-12-09 20:01:43 +0100

    added some functions

--- sorting/src/SortingVisualizer/SortingVisualizer.tsx
+++ sorting/src/SortingVisualizer/SortingVisualizer.tsx
@@ -1,6 +1,23 @@
 import React, { useState } from 'react'
 
 function SortingVisualizer() {
+    const [array, setArray] = useState([])
+
+    function randomIntFromInterval(min: number, max: number) {
+        return Math.floor(Math.random() * (max - min + 1)); //takes the min and max value and returns random ints between
+    }
+
+    function resetArray() {
+        setArray([])
+        const temp_array:any = []; //this is any because i couldn't get it to work with number
+        for (let i = 0;
+            i > 100;
+            i++){
+                temp_array.push(randomIntFromInterval(5, 1000));
+            }
+            setArray(temp_array)
+    }
+
     return (
         <div>
             lol
@@ -8,4 +25,4 @@ function SortingVisualizer() {
     )
 }
 
-export default SortingVisualizer
+export default SortingVisualizer;
