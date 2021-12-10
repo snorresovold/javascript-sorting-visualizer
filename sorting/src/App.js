4f40be7f5e9f80aa801c530ff7da7ba5b6ea8bf4
snorresovold <snorresovold@gmail.com>
2021-12-10 16:49:24 +0100

    done shit

---
+++ sorting/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SortingVisualizer from './SortingVisualizer/SortingVisualizer';
+
+function App() {
+  return (
+    <div className="App">
+      <SortingVisualizer />
+    </div>
+  );
+}
+
+export default App;
